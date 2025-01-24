c28bc3a2b30427e58a51746a3a23dfa4f50df358
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2025-01-24 10:11:55 -0800

    Add files via upload

---
+++ aoc1.1.cs
@@ -0,0 +1,36 @@
+﻿﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AocDay1._1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            foreach (string s in System.IO.File.ReadLines("input1.1.txt"))
+            {
+                string[] both = s.Split("   ");
+                left.Add(Int32.Parse(both[0]));
+                right.Add(Int32.Parse(both[1]));
+            }
+
+            if (left.Count != right.Count)
+            {
+                throw new InvalidDataException("Not good yo");
+            }
+
+            left.Sort();
+            right.Sort();
+            int totalDistance = 0;
+            for (int i = 0; i < left.Count; ++i)
+            {
+                totalDistance += Math.Abs(left[i] - right[i]);
+            }
+            Console.WriteLine(totalDistance);
+        }
+    }
+}
