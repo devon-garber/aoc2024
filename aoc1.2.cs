70798a740c5f62b6eba4944435139c5cc2dd9fec
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2025-01-24 10:20:42 -0800

    Add files via upload

---
+++ aoc1.2.cs
@@ -0,0 +1,42 @@
+﻿﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AocDay1._1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            foreach (string s in System.IO.File.ReadLines("input1.1.txt"))
+            {
+                string[] both = s.Split("   ");
+                left.Add(Int32.Parse(both[0]));
+                right.Add(Int32.Parse(both[1]));
+            }
+
+            if (left.Count != right.Count)
+            {
+                throw new InvalidDataException("Not good yo");
+            }
+
+            int similarityScore = 0;
+            for (int i = 0; i < left.Count; ++i)
+            {
+                int numInRight = 0;
+                for (int j = 0; j < right.Count; ++j)
+                {
+                    if (left[i] == right[j])
+                    {
+                        numInRight++;
+                    }
+                }
+                similarityScore += numInRight * left[i];
+            }
+            Console.WriteLine(similarityScore);
+        }
+    }
+}
